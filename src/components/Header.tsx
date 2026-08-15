947c1814409867e73f514250dce04bf8745505c0
NickyHariniaina <ferrissushi@gmail.com>
2026-08-15 15:47:46 +0300

    feat: use profile picture as header avatar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,14 @@
 import { site } from '../data'
+import profilPic from '../assets/profil-pic.png'
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="header__avatar" aria-hidden="true">
-        NH
-      </div>
+      <img
+        src={profilPic}
+        alt="Nicky Hariniaina"
+        className="header__avatar"
+      />
       <h1 className="header__name">{site.name}</h1>
       <p className="header__role">&lt;&gt; {site.role} &lt;/&gt;</p>
     </header>
